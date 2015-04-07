fed0664aafe9b566e896108a44f0d6c11c9698f1
azzot88 <azzot88@gmail.com>
2015-04-07 19:07:23 +0300

    Дз на проверку

--- LessonThree2.0/ViewController.m
+++ LessonThree2.0/ViewController.m
@@ -10,9 +10,12 @@
 
 @interface ViewController ()
 
-@property (nonatomic, strong) NSMutableArray * arrayM;
-@property (nonatomic, strong) NSArray * arrayPrices;
-@property (nonatomic, strong) NSArray * arrayValues;
+
+@property (nonatomic, strong) NSMutableArray * arrayBase;
+@property (nonatomic, strong) NSArray * arrayNicknames;
+@property (nonatomic, strong) NSArray * arrayRooms;
+//@property (nonatomic, strong) NSArray * arrayBalances;
+//Хотел сделать три ячейки - не хватило времени разобраться.
 
 
 @end
@@ -23,27 +26,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSString * stringPrices = @"325,1013,99";
-    NSString * stringValues = @"Тетрадь,Телевизор,Утюг";
+//<<<<<<< Updated upstream
+//    NSString * stringPrices = @"325,1013,99";
+//    NSString * stringValues = @"Тетрадь,Телевизор,Утюг";
+//    
+//    self.arrayPrices = [stringPrices componentsSeparatedByString:@","];
+//    self.arrayValues = [stringValues componentsSeparatedByString:@","];
+//    
+//    
+//    
+//    
+//    
+//    self.arrayM = [NSMutableArray array];
+//    
+//    NSInteger i = 17;
+//    
+//    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+//                           @"string1", @"str1",
+//                           @"string2", @"str2",
+//                           @"string3", @"str3",
+//                           @(i), @"int", nil];
+//    
+//    NSLog(@"dict %@", dict);
+//=======
+    NSString * stringNicknames = @"Mr. Smith,Mao Tse-Tung,Kazemir Malevich,Alex Pushkin,Kastro Fidel,Elon Musk";
+    NSString * stringRooms = @"325,311,201,438,367,211";
+  //  NSString * stringBalances = @"24,3,256,453,2301,14000260";
     
-    self.arrayPrices = [stringPrices componentsSeparatedByString:@","];
-    self.arrayValues = [stringValues componentsSeparatedByString:@","];
+    self.arrayBase = [NSMutableArray array];
+    
+    self.arrayNicknames = [stringNicknames componentsSeparatedByString:@","];
+    self.arrayRooms = [stringRooms componentsSeparatedByString:@","];
+   // self.arrayBalances = [stringBalances componentsSeparatedByString:@","];
     
     
+    for (int i = 0; i < self.arrayNicknames.count; i++) {
+        
+        NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+                               [self.arrayNicknames objectAtIndex:i], @"name",
+                               [self.arrayRooms objectAtIndex:i], @"room", nil];
+//                               [self.arrayBalances objectAtIndex:i], @"balance",
+        
+        [self.arrayBase addObject:dict];
+        
+    }
+    
+    NSLog(@"self.arrayBase %@", self.arrayBase);
+    
+//    NSInteger i = 17;
+    
+//    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+//                           @"string1", @"str1",
+//                           @"string2", @"str2",
+//                           @"string3", @"str3",
+//                           @(i), @"int", nil];
     
     
-    
-    self.arrayM = [NSMutableArray array];
-    
-    NSInteger i = 17;
-    
-    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
-                           @"string1", @"str1",
-                           @"string2", @"str2",
-                           @"string3", @"str3",
-                           @(i), @"int", nil];
-    
-    NSLog(@"dict %@", dict);
+//>>>>>>> Stashed changes
     
 }
 
@@ -63,22 +102,61 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.arrayPrices.count ;
+
+    return self.arrayBase.count ;
+
 
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+//<<<<<<< Updated upstream
+//=======
+    
+//    Player *player = (self.players)[indexPath.row];
+//    
+//    UILabel *nameLabel = (UILabel *)[cell viewWithTag:100];
+//    nameLabel.text = player.name;
+//    
+//    UILabel *gameLabel = (UILabel *)[cell viewWithTag:101];
+//    gameLabel.text = player.game;
+//    
+//    UIImageView *ratingImageView = (UIImageView *)[cell viewWithTag:102];
+//    ratingImageView.image = [self imageForRating:player.rating];
+    
+    
+
     static NSString * simpleTableIdentifier = @"Cell";
     UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
     }
-    cell.textLabel.text = [self.arrayValues objectAtIndex:indexPath.row];
-    cell.detailTextLabel.text = [self.arrayPrices objectAtIndex:indexPath.row];
+
+//    cell.textLabel.text = [self.arrayValues objectAtIndex:indexPath.row];
+//    cell.detailTextLabel.text = [self.arrayPrices objectAtIndex:indexPath.row];
+//    return cell;
+    
+//}
+
+    cell.textLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"name"];
+    cell.detailTextLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"room"];
+//    cell.detailTextLabel.text = [[self.arrayBase objectAtIndex:indexPath.row]objectForKey:@"balance"];
+    
     return cell;
     
 }
+//- (UIImage *)imageForRating:(int)rating
+//{
+//    switch (rating) {
+//        case 1: return [UIImage imageNamed:@"1StarSmall"];
+//        case 2: return [UIImage imageNamed:@"2StarsSmall"];
+//        case 3: return [UIImage imageNamed:@"3StarsSmall"];
+//        case 4: return [UIImage imageNamed:@"4StarsSmall"];
+//        case 5: return [UIImage imageNamed:@"5StarsSmall"];
+//    }
+//    return nil;
+//}
+//>>>>>>> Stashed changes
 
 - (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
 {
